eae614e85276120fd55403c15422e3f787ae0cec
chigozieorunta <chigozieorunta@yahoo.com>
2019-03-23 11:43:34 +0100

    Modified index dashboard page to reflex canvas design

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Drago | Build Websites the Simplified Way!</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/drago.css" rel="stylesheet">
+    </head>
+    <body>
+        <section class="drago-fullscreen">
+            <div class="drago-fullscreen-child">
+                <div class="container-fluid drago-fullheight">
+                    <div class="row fullheight">
+                        <div class="col-sm-9 col-sm-offset-3 drago-fullheight drago-canvas-bg">
+                            <div style="padding: 50px;">
+                                <div class="drago-canvas">
+                                    <div class="drago-section drago-component">
+                                        <h2>The Word</h2>
+                                        <span contenteditable="true" style="border: none !important;">She added: "I used to feel like I could only wear a certain type of clothing and I think sometimes you just get stuck in a bit of a rut and wear the same thing over and and over again and you create rules for yourself, you think 'ooh I can't wear trousers because I've got a big bum' and you create some sort of rule book of your own and sometimes you've just got to go right, I'm going to tear up that rule book and I'm going to experiment...</span>
+                                    </div>
+                                    <div class="drago-section drago-component">
+                                        <div class="row">
+                                            <div class="col-sm-4 drago-component">I could only wear a certain type of clothing and I think sometimes you just get stuck in a bit of a rut and wear the same thing over and and over again and you create rules for yourself, you think 'ooh I can't wear trousers because I've got a big bum' and you create some sort of rule book</div>
+                                            <div class="col-sm-4 drago-component">I could only wear a certain type of clothing and I think sometimes you just get stuck in a bit of a rut and wear the same thing over and and over again and you create rules for yourself, you think 'ooh I can't wear trousers because I've got a big bum' and you create some sort of rule book</div>
+                                            <div class="col-sm-4 drago-component">I could only wear a certain type of clothing and I think sometimes you just get stuck in a bit of a rut and wear the same thing over and and over again and you create rules for yourself, you think 'ooh I can't wear trousers because I've got a big bum' and you create some sort of rule book</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </body>
+</html>
